9d969b2eed2b764960266278e2334585db36266e
Adrian Gomez <adrian.gomez.garlito@gmail.com>
2024-03-07 11:04:08 +0100

    empezado conexion a API

--- app/src/main/java/com/example/pokedexfinal/api/ApiService.kt
+++ app/src/main/java/com/example/pokedexfinal/api/ApiService.kt
@@ -1,4 +1,20 @@
 package com.example.pokedexfinal.api
 
+import com.example.pokedexfinal.data.Pokemon
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("pokemon/{id}")
+    fun getPokemonInfo(@Path("id") id:Int): Call<Pokemon>
+
+    @GET("pokemon")
+    fun getPokemonList(@Query("limit")limit:Int,@Query("Offset") offset: Int):Call<PokeApiResponse>
+
+    @GET("pokemon-species/{id}")
+    fun getPokemonSpecies(@Path("id")id:Int):Call<Pokemon>
+
 }
